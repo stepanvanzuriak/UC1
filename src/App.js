3164c8ab087c418b945dceb8b7b75bd09c90119c
Stepan Vanzuriak <stepanvanzuriak@gmail.com>
2023-09-03 20:49:41 +0300

    UC#1 Create endpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(countries);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get('https://restcountries.com/v3.1/all');
+
+      setCountries(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={onSubmit} className="main-form">
+      <input placeholder="Name" />
+      <input placeholder="Population" />
+      <input placeholder="Sort by country" />
+      <input placeholder="Number of records" />
+      <input type="submit" />
+    </form>
   );
-}
+};
 
 export default App;
